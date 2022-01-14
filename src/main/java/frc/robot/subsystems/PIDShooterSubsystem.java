35f0331068feee9dda02f10a948ba6efa9badd55
Rishi Wadhwa <rishi@wadhwas.us>
2022-01-14 15:58:04 -0800

    PID Shooter Subsystem created

---
+++ src/main/java/frc/robot/subsystems/PIDShooterSubsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PIDShooterSubsystem extends SubsystemBase {
+    public PIDShooterSubsystem() {}
+
+
+}
