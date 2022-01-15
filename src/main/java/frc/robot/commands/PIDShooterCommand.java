5169ebe8674e0f4b186b4701d58a2623326eca6f
Rishi Wadhwa <rishi@wadhwas.us>
2022-01-14 16:11:11 -0800

    Switch to PIDSubsystem from SubsystemBase

--- src/main/java/frc/robot/commands/PIDShooterCommand.java
+++ src/main/java/frc/robot/commands/PIDShooterCommand.java
@@ -17,6 +17,8 @@ public class PIDShooterCommand extends CommandBase {
 
     }
 
+
+
     @Override
     public boolean isFinished() {
         return true;
--- src/main/java/frc/robot/subsystems/PIDShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/PIDShooterSubsystem.java
@@ -1,9 +1,21 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 
-public class PIDShooterSubsystem extends SubsystemBase {
-    public PIDShooterSubsystem() {}
+public class PIDShooterSubsystem extends PIDSubsystem {
 
+    public PIDShooterSubsystem(PIDController controller, double initialPosition) {
+        super(controller, initialPosition);
+    }
 
+    @Override
+    protected void useOutput(double output, double setpoint) {
+
+    }
+
+    @Override
+    protected double getMeasurement() {
+        return 0;
+    }
 }
