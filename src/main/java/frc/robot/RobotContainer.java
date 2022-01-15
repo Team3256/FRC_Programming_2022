68dacb8dc11e393472e99a7d0cd1edf2a49924de
RoboticsComp3 <robotics@warriorlife.net>
2022-01-15 14:43:03 -0800

    convert limelight to command based

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,10 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.TestLimelight;
 import frc.robot.helper.logging.RobotLogger;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -24,6 +23,9 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   public final Limelight limelight = new Limelight();
+
+  //For Testing
+  private TalonFX test = new TalonFX(3);
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
