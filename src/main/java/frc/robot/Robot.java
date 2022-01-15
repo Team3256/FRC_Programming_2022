5e28e0725f940fc2295aa083a0e628702ecb3359
3LucasZ <lucas.zheng@warriorlife.net>
2022-01-15 11:50:16 -0800

    Add update

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.TestCANCommand;
+import frc.robot.hardware.Limelight;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,9 +18,8 @@ import frc.robot.commands.TestCANCommand;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
-
+  private Limelight limelight;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -29,6 +29,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    limelight = Limelight.getInstance();
+    limelight.init();
   }
 
   /**
@@ -45,6 +47,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    limelight.update(0);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
--- src/main/java/frc/robot/hardware/Limelight.java
+++ src/main/java/frc/robot/hardware/Limelight.java
@@ -4,12 +4,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.team3256.warriorlib.loop.Loop;
 
-public class Limelight {
+public class Limelight implements Loop {
     private NetworkTableInstance inst;
     private NetworkTableEntry limeLightTx, limeLightTy, limeLightTa, limeLightTs, limeLightTcornx, limeLightTcorny;
     private double tx, ty, ta, ts;
     private double [] tcornx, tcorny;
+
+    //singleton
+    private static Limelight instance;
+    public static Limelight getInstance() { return instance == null ? instance = new Limelight() : instance; }
 
     public void init() {
         //Setting up NetworkTables
@@ -30,5 +35,23 @@ public class Limelight {
         limelight.getEntry("pipeline").setNumber(0); //Uses pipeline #0
         limelight.getEntry("stream").setNumber(2); //Driver Camera Main, Vision Camera Lower-Right Corner
         limelight.getEntry("snapshot").setNumber(0); //Takes no snapshots
+    }
+    @Override
+    public void update(double timestamp) {
+        tx = limeLightTx.getDouble(0);
+        ty = limeLightTy.getDouble(0);
+        ta = limeLightTa.getDouble(0);
+        ts = limeLightTs.getDouble(0);
+        tcornx = limeLightTcornx.getDoubleArray(new double[4]);
+        tcorny = limeLightTcorny.getDoubleArray(new double[4]);
+    }
+
+    @Override
+    public void init(double timestamp) {
+
+    }
+    @Override
+    public void end(double timestamp) {
+
     }
 }
