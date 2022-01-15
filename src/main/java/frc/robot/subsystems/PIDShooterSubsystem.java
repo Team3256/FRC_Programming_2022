a541c28a65b9f7b26f004092c290dcf7429f7502
Rishi Wadhwa <rishi@wadhwas.us>
2022-01-14 16:33:19 -0800

    PID Shooter Motor Port Added

--- src/main/java/frc/robot/subsystems/PIDShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/PIDShooterSubsystem.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
+import frc.robot.Constants;
 
 public class PIDShooterSubsystem extends PIDSubsystem {
+    private final TalonFX m_shooterMotor = new TalonFX(Constants.IDConstants.PID_SHOOTER_MOTOR_ID);
 
     public PIDShooterSubsystem(PIDController controller, double initialPosition) {
         super(controller, initialPosition);
@@ -11,7 +14,11 @@ public class PIDShooterSubsystem extends PIDSubsystem {
 
     @Override
     protected void useOutput(double output, double setpoint) {
+        // leave blank
+    }
 
+    protected void setSpeed(double speed) {
+//        m_shooterMotor.set();
     }
 
     @Override
