f1f032a6c392f20cb20b945b738a1187d316e9be
William Chen <william.chen2@warriorlife.net>
2022-01-22 14:13:08 -0800

    javadocs for Limelight

--- src/main/java/frc/robot/helper/Limelight.java
+++ src/main/java/frc/robot/helper/Limelight.java
@@ -21,33 +21,66 @@ public class Limelight {
         limelight.getEntry("stream").setNumber(2); //Driver Camera Main, Vision Camera Lower-Right Corner
         limelight.getEntry("snapshot").setNumber(0); //Takes no snapshots
     }
+
     private static NetworkTableEntry getLimelightValue(String value){
         return limelight.getEntry(value);
     }
+
+    /**
+     * @return Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+     */
     public static double getTx(){
         return getLimelightValue("tx").getDouble(0);
     }
+
+    /**
+     * @return Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
+     */
     public static double getTy(){
         return getLimelightValue("ty").getDouble(0);
     }
+
+    /**
+     * @return Target Area (0% of image to 100% of image)
+     */
     public static double getTa(){
         return getLimelightValue("ta").getDouble(0);
     }
+
+    /**
+     * @return Skew or rotation (-90 degrees to 0 degrees)
+     */
     public static double getTs(){
         return getLimelightValue("ts").getDouble(0);
     }
+
+    /**
+     * @return Number array of corner coordinates [x0,x1……]
+     */
     public static double[] getTcornx(){
         return getLimelightValue("tcornx").getDoubleArray(new double[4]);
     }
+
+    /**
+     * @return Number array of corner coordinates [y0,y1……]
+     */
     public static double[] getTcorny(){
         return getLimelightValue("tcorny").getDoubleArray(new double[4]);
     }
+
     public static double getDistanceToTarget(){
         return (TARGET_HEIGHT_INCHES-MOUNTING_HEIGHT_INCHES)/Math.tan(toRadians(MOUNTING_ANGLE_DEG+getTy()));
     }
+
     public static double toRadians(double degrees){
         return degrees * Math.PI/180.0;
     }
+
+    /**
+     * @param x the not tuned distance
+     * tunes the distance you put in to the actual distance
+     * @return the actual distance
+     */
     public static double getTuned(double x){
         return 1.04*x + -9.54;
     }
