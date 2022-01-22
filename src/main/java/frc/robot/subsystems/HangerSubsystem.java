b5b8d1f75ad2bf28f2201d069498ae42c411e993
Silverleaf31 <silverleaf.yen@gmail.com>
2022-01-21 17:48:52 -0800

    set hanger motor speeds

--- src/main/java/frc/robot/subsystems/HangerSubsystem.java
+++ src/main/java/frc/robot/subsystems/HangerSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -22,10 +23,12 @@ public class HangerSubsystem extends SubsystemBase {
     }
 
     public void hangerMotorSpool() {
+        hangerMotor.set(SPOOL_FORWARD_SPEED);
 
     }
 
     public void hangerMotorUnspool() {
+        hangerMotor.set(SPOOL_BACKWARD_SPEED);
 
     }
 
