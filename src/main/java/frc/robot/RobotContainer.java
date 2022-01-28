904d6ca34a7df54d16ee818c3ec4db0a58af2281
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-01-28 15:18:47 -0800

    Cubed the value to add dampening

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -124,6 +124,7 @@ public class RobotContainer {
     private static double modifyAxis(double value) {
         // Deadband
         value = deadband(value, 0.05);
+        value=value*value*value;
 
         // Square the axis
         value = Math.copySign(value * value, value);
