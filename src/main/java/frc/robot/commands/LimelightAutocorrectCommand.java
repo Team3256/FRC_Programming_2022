adadcab376230de2c16b3525636e73d28707ed5a
3LucasZ <lucas.j.zheng@gmail.com>
2022-02-09 18:39:28 -0800

    Constants update, Limelight Autocorrect command, Limelight Autocorrect step command

---
+++ src/main/java/frc/robot/commands/LimelightAutocorrectCommand.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.SwerveDrive;
+import org.apache.commons.math3.fitting.PolynomialCurveFitter;
+import org.apache.commons.math3.fitting.WeightedObservedPoints;
+
+import static frc.robot.Constants.LimelightAutoCorrectConstants.*;
+
+public class LimelightAutocorrectCommand extends CommandBase {
+    WeightedObservedPoints data;
+    final SwerveDrive robotDrive;
+    int counter;
+    final int initDistance;
+    int curDistance;
+
+    final PolynomialCurveFitter fitter = PolynomialCurveFitter.create(5);
+
+    public LimelightAutocorrectCommand(SwerveDrive robotDrive, int initDistance){
+        this.robotDrive = robotDrive;
+        this.initDistance = initDistance;
+    }
+
+    @Override
+    public void initialize() {
+        counter = 0;
+        data = new WeightedObservedPoints();
+        curDistance = initDistance;
+    }
+
+    @Override
+    public void execute(){
+        if (!CommandScheduler.getInstance().isScheduled()) {
+            CommandScheduler.getInstance().schedule(new LimelightAutocorrectStepCommand(robotDrive, data, curDistance));
+            counter++;
+            curDistance-=PACE_SIZE;
+        }
+    }
+    @Override
+    public boolean isFinished(){
+        return counter >= PACES;
+    }
+}
---
+++ src/main/java/frc/robot/helper/Function.java
@@ -0,0 +1,5 @@
+package frc.robot.helper;
+
+public class Function {
+    
+}
