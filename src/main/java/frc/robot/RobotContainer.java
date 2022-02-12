0f60c6fe111d47b029acb5cb50bc3b3a500256e0
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2022-02-12 12:34:55 -0800

    Hanger (#19)
    * Created HangerSubsystem.java
    
    * Contructor hanger subsystem
    
    * Created hangerMotor and pneumaticFire functions
    
    * Created 2 hangerMotor and 2 pneumaticFire functions
    
    * Create Double Solenoid
    
    * set hanger motor speeds
    
    * Hanger Constants
    
    * Update HangerSubsystem.java spool and unspool
    
    * cleaned up hanger code
    
    Co-authored-by: abalakrishnan1 <a.balakrishnan@warriorlife.net>
    
    * added function to stop motor when current surpasses threshold, configured bindings for hanger subsystem
    
    Co-authored by: abalakrishnan1 <a.balakrishnan@warriorlife.net>
    
    * add hanger commands
    
    Co-authored by: abalakrishnan1 <a.balakrishnan@warriorlife.net>
    
    * added support for another motor
    
    Co-authored by: abalakrishnan1 <a.balakrishnan@warriorlife.net>
    
    * cleaned up code and fixed naming conventions
    
    * fixes
    
    * refactored code to @StarDylan's specs
    
    * updated code to meet specs
    
    * Moved Hanger Commands to own Package
    
    * Added More Specific Comments
    
    Changing the Units from Meters to Rotations
    of the spool that holds the string.
    
    * Retract is Opposite from Extend
    
    * added config PID constants
    
    * Fix Issues from Merge
    
    * Disallow Instancing from Limelight
    
    * switched from stopping by exceeding current threshold to if limit switch is triggered
    
    * Added an Auto Sequential command for the Hanger
    Co-authored-by: akhil-datla <66145155+akhil-datla@users.noreply.github.com>
    
    * added support for air brake
    
    * fix autonomous hang and wait times
    
    * fixup! Merge branch 'hanger' of github.com-akhil-datla:Team3256/FRC_Programming_2022 into hanger
    
    * fixup! added support for air brake
    
    Co-authored-by: Silverleaf31 <silverleaf.yen@gmail.com>
    Co-authored-by: Rishi Wadhwa <rishi@wadhwas.us>
    Co-authored-by: abalakrishnan1 <a.balakrishnan@warriorlife.net>
    Co-authored-by: Dylan Starink <dylan@starink.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,9 +16,12 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.robot.auto.AutoChooser;
 import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.commands.hanger.AutoHang;
+import frc.robot.subsystems.HangerSubsystem;
 import frc.robot.subsystems.SwerveDrive;
 import frc.robot.Constants.SwerveConstants;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -41,87 +44,97 @@ import java.util.Set;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  public final Limelight limelightSubsystem = new Limelight();
-  private final SwerveDrive drivetrainSubsystem = new SwerveDrive();
-  private final IntakeSubsystem intake = new IntakeSubsystem();
-  private final FlywheelSubsystem flywheelSubsystem = new FlywheelSubsystem();
-  
-  private final Field2d field = new Field2d();
-  
-  private final XboxController controller = new XboxController(0);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-      Limelight.init();
-    RobotLogger.setup();
-      // Set up the default command for the drivetrain.
-      // The controls are for field-oriented driving:
-      // Left stick Y axis -> forward and backwards movement
-      // Left stick X axis -> left and right movement
-      // Right stick X axis -> rotationx
+    // The robot's subsystems and commands are defined here...
+    private final SwerveDrive drivetrainSubsystem = new SwerveDrive();
+    private final HangerSubsystem hanger = new HangerSubsystem();
+    private final IntakeSubsystem intake = new IntakeSubsystem();
+    private final FlywheelSubsystem flywheelSubsystem = new FlywheelSubsystem();
 
-     drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
-              drivetrainSubsystem,
-              () -> -modifyAxis(controller.getLeftY()) * SwerveConstants.MAX_VELOCITY_METERS_PER_SECOND,
-              () -> -modifyAxis(controller.getLeftX()) * SwerveConstants.MAX_VELOCITY_METERS_PER_SECOND,
-              () -> -modifyAxis(controller.getRightX()) * SwerveConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
-     ));
-       
-     configureButtonBindings();
-  }
+    private final Field2d field = new Field2d();
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    Button rightBumper = new JoystickButton(controller, XboxController.Button.kRightBumper.value);
+    private final XboxController controller = new XboxController(0);
+
+    /**
+     *
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+      RobotLogger.setup();
+
+        Limelight.init();
+        // Set up the default command for the drivetrain.
+        // The controls are for field-oriented driving:
+        // Left stick Y axis -> forward and backwards movement
+        // Left stick X axis -> left and right movement
+        // Right stick X axis -> rotationx
+
+       drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+                drivetrainSubsystem,
+                () -> -modifyAxis(controller.getLeftY()) * SwerveConstants.MAX_VELOCITY_METERS_PER_SECOND,
+                () -> -modifyAxis(controller.getLeftX()) * SwerveConstants.MAX_VELOCITY_METERS_PER_SECOND,
+                () -> -modifyAxis(controller.getRightX()) * SwerveConstants.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+        ));
+
+        // Configure the button bindings
+        configureButtonBindings();
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        Button rightBumper = new JoystickButton(controller, XboxController.Button.kRightBumper.value);
+
         // Back button zeros the gyroscope
         new Button(controller::getAButton)
                 // No requirements because we don't need to interrupt anything
                 .whenPressed(drivetrainSubsystem::zeroGyroscope);
+        new Button(controller::getStartButton)
+                .whenPressed(new AutoHang(hanger));
 
-      rightBumper.whenHeld(new IntakeOn(intake));
+        rightBumper.whenHeld(new IntakeOn(intake));
     }
+
     public SendableChooser<Command> getCommandChooser() {
         return AutoChooser.getDefaultChooser(drivetrainSubsystem);
     }
 
-  public Trajectory getTrajectory() { // FIXME: scuffed rn, pls fix later
-      TrajectoryConfig config =
+    public Trajectory getTrajectory() { // FIXME: scuffed rn, pls fix later
+        TrajectoryConfig config =
               new TrajectoryConfig(
                       Constants.AutoConstants.MAX_SPEED_CONTROLLER_METERS_PER_SECOND,
                       Constants.AutoConstants.MAX_ACCELERATION_CONTROLLER_METERS_PER_SECOND_SQUARED)
                       // Add kinematics to ensure max speed is actually obeyed
                       .setKinematics(drivetrainSubsystem.getKinematics());
 
-      List<Pose2d> waypoints = new ArrayList<>();
-      for(int pos = 0; pos <= 80; pos++){
-          waypoints.add(new Pose2d(Units.inchesToMeters(pos), 0, new Rotation2d()));
-      }
+        List<Pose2d> waypoints = new ArrayList<>();
+        for(int pos = 0; pos <= 80; pos++){
+            waypoints.add(new Pose2d(Units.inchesToMeters(pos), 0, new Rotation2d()));
+        }
         // List<Translation2d> waypoints = List.of(new Translation2d(Units.inchesToMeters(12), 0));s
         // JSONReader.ParseJSONFile("");
 
         // An example trajectory to follow.  All units in meters.
-      Trajectory trajectory1 =
+        Trajectory trajectory1 =
               TrajectoryGenerator.generateTrajectory(
                       // Start at the origin facing the +X direction
                       waypoints,
                       config);
 
-      return trajectory1;
+        return trajectory1;
     }
 
-  private void configureShooter() {
-    XboxController xboxController = new XboxController(0);
+    private void configureShooter() {
+        JoystickAnalogButton rightTrigger = new JoystickAnalogButton(controller, XboxController.Axis.kRightTrigger.value);
+        rightTrigger.setThreshold(0.01);
 
-    JoystickAnalogButton rightTrigger = new JoystickAnalogButton(xboxController, XboxController.Axis.kRightTrigger.value);
-    rightTrigger.setThreshold(0.01);
+        rightTrigger.whenPressed(new SetShooterFromTriggerDebug(flywheelSubsystem, controller::getRightTriggerAxis));
 
-    rightTrigger.whenPressed(new SetShooterFromTriggerDebug(flywheelSubsystem, xboxController::getRightTriggerAxis));
-  }
+    }
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
---
+++ src/main/java/frc/robot/subsystems/HangerSubsystem.java
@@ -0,0 +1,123 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.HangerConstants.*;
+import static frc.robot.Constants.IDConstants.*;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
+public class HangerSubsystem extends SubsystemBase {
+    private final TalonFX masterTalonMotor;
+    private final TalonFX followerTalonMotor;
+    private final DoubleSolenoid leftSolenoid;
+    private final DoubleSolenoid rightSolenoid;
+    private final DoubleSolenoid leftAirBrake;
+    private final DoubleSolenoid rightAirBrake;
+    DigitalInput bottomLimitSwitch = new DigitalInput(LIMIT_SWITCH_CHANNEL);
+
+
+    public HangerSubsystem() {
+        masterTalonMotor = new TalonFX(HANGER_MASTER_TALON_ID);
+        followerTalonMotor = new TalonFX(HANGER_FOLLOWER_TALON_ID);
+
+        masterTalonMotor.config_kP(0, HANGER_MASTER_TALON_PID_P); //TODO: change slotIdx if required
+        masterTalonMotor.config_kI(0, HANGER_MASTER_TALON_PID_I); //TODO: change slotIdx if required
+        masterTalonMotor.config_kD(0, HANGER_MASTER_TALON_PID_D); //TODO: change slotIdx if required
+        masterTalonMotor.config_kF(0, HANGER_MASTER_TALON_PID_F); //TODO: change slotIdx if required
+
+        masterTalonMotor.setInverted(INVERT_MOTOR);
+
+        followerTalonMotor.follow(masterTalonMotor);
+        followerTalonMotor.setInverted(InvertType.OpposeMaster);
+
+        masterTalonMotor.setNeutralMode(NeutralMode.Brake);
+        followerTalonMotor.setNeutralMode(NeutralMode.Brake);
+
+        leftSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, HANGER_SOLENOID_LEFT_FORWARD, HANGER_SOLENOID_LEFT_BACKWARD);
+        rightSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, HANGER_SOLENOID_RIGHT_FORWARD, HANGER_SOLENOID_RIGHT_BACKWARD);
+        leftAirBrake = new DoubleSolenoid(PneumaticsModuleType.REVPH, HANGER_SOLENOID_LEFT_AIRBRAKE_FORWARD, HANGER_SOLENOID_LEFT_AIRBRAKE_BACKWARD);
+        rightAirBrake = new DoubleSolenoid(PneumaticsModuleType.REVPH, HANGER_SOLENOID_RIGHT_AIRBRAKE_FORWARD, HANGER_SOLENOID_RIGHT_AIRBRAKE_BACKWARD);
+        engageAirBrake();
+   }
+    public void engageAirBrake() {
+        leftAirBrake.set(kForward);
+        rightAirBrake.set(kForward);
+        //TODO: CHECK THIS
+    }
+
+    public void disengageAirBrake() {
+        leftAirBrake.set(kReverse);
+        rightAirBrake.set(kReverse);
+        //TODO: CHECK THIS
+    }
+
+    public void extend() {
+        double distance = EXTEND_DISTANCE * 2048 * GEAR_RATIO;
+        masterTalonMotor.set(ControlMode.Position, distance);
+    }
+
+    public void retractContinuously() {
+        masterTalonMotor.set(ControlMode.PercentOutput, -1 * RETRACT_PERCENT_SPEED);
+    }
+
+    public void extendPartial() {
+        double distance = PARTIAL_DISTANCE * 2048 * GEAR_RATIO;
+        masterTalonMotor.set(ControlMode.Position, distance);
+    }
+
+    public void pneumaticUpright() {
+        leftSolenoid.set(kForward);
+        rightSolenoid.set(kForward);
+    }
+
+    public void pneumaticSlant() {
+        leftSolenoid.set(kReverse);
+        rightSolenoid.set(kReverse);
+    }
+
+    /**
+     * check if the bottom limit switch is triggered
+     * @return returns if the bottom limit switch is triggered
+     */
+    public boolean hasReachedBottom() {
+        if (bottomLimitSwitch.get()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * get the position of master talon in rotations of spool
+     * @return returns position of master talon in rotations of spool
+     */
+    public double getPosition() {
+        return (masterTalonMotor.getSelectedSensorPosition()/2048)/GEAR_RATIO;
+    }
+
+    /**
+     * check if master talon motor has reached the full intended distance
+     * @return returns true if master talon has the full reached intended distance
+     */
+    public boolean isFullPositionReached() {
+        return getPosition() >= EXTEND_DISTANCE;
+    }
+
+    /**
+     * check if master talon motor has reached the partial intended distance
+     * @return returns true if master talon has the partial reached intended distance
+     */
+    public boolean isPartialPositionReached() {
+        return getPosition() >= PARTIAL_DISTANCE;
+    }
+
+    public void stopMotors() {
+        masterTalonMotor.set(ControlMode.PercentOutput, 0);
+    }
+
+}
