3e29dea39799e873c6cbf01505c2f67173e567ac
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-02-11 18:02:33 -0800

    Add initial rotation to UniformThetaSupplier

--- src/main/java/frc/robot/helper/UniformThetaSupplier.java
+++ src/main/java/frc/robot/helper/UniformThetaSupplier.java
@@ -6,7 +6,15 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class UniformThetaSupplier implements ThetaSupplier {
     private double trajectoryDuration;
     private Rotation2d desiredRotation;
+    private Rotation2d initialAngle = new Rotation2d();
     private double proportion;
+
+    public UniformThetaSupplier(double trajectoryDuration, Rotation2d desiredRotation, Rotation2d initialAngle, double proportion) {
+        this.trajectoryDuration = trajectoryDuration;
+        this.desiredRotation = desiredRotation;
+        this.initialAngle = initialAngle;
+        this.proportion = proportion;
+    }
 
     public UniformThetaSupplier(double trajectoryDuration, Rotation2d desiredRotation, double proportion) {
         this.trajectoryDuration = trajectoryDuration;
@@ -26,16 +34,28 @@ public class UniformThetaSupplier implements ThetaSupplier {
         this.proportion = proportion;
     }
 
+    public UniformThetaSupplier(Rotation2d desiredRotation, Rotation2d initialAngle, double proportion) {
+        this.trajectoryDuration = 5; // SHOULD GET CHANGED BY TRAJECTORY FACTORY
+        this.desiredRotation = desiredRotation;
+        this.initialAngle = initialAngle;
+        this.proportion = proportion;
+    }
+
     public void setTrajectoryDuration(double duration) {
         this.trajectoryDuration = duration;
     }
 
     public Rotation2d rotationSupply(double now) {
-        return new Rotation2d((this.desiredRotation.getRadians() >= 0 ? 1 : -1) * Math.min(
-                Math.abs(
-                        this.desiredRotation.getRadians() * (now/(this.trajectoryDuration * proportion))),
-                Math.abs(
-                        this.desiredRotation.getRadians())
+        return new Rotation2d((this.desiredRotation.getRadians() >= 0 ? 1 : -1) *
+                (this.initialAngle.getRadians()
+                + Math.min(
+                    Math.abs(
+                            this.desiredRotation.getRadians() * (now/(this.trajectoryDuration * proportion))
+                    ),
+                    Math.abs(
+                            this.desiredRotation.getRadians()
+                    )
+                )
         ));
     }
 }
