d6259136d2897c7c45daf31ec02c4fb76f2e7eaa
3LucasZ <lucas.j.zheng@gmail.com>
2022-02-16 19:05:05 -0800

    remove execute on zerohoodmotor cmd

--- src/main/java/frc/robot/commands/ZeroHoodMotor.java
+++ src/main/java/frc/robot/commands/ZeroHoodMotor.java
@@ -19,12 +19,6 @@ public class ZeroHoodMotor extends CommandBase {
         flywheel.hoodSlowReverse();
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-
-    }
-
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
