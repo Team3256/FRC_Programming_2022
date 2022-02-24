d1879f23f3b277ca67af255804723a1215ff00ec
3LucasZ <lucas.zheng@warriorlife.net>
2022-02-23 16:11:03 -0800

    Create HangerAlignTwo.java

---
+++ src/main/java/frc/robot/commands/hanger/HangerAlignTwo.java
@@ -0,0 +1,42 @@
+package frc.robot.commands.hanger;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.helper.MuxedColorSensor;
+import frc.robot.subsystems.SwerveDrive;
+
+import static frc.robot.Constants.HangerConstants.TAPE_COLOR;
+
+public class HangerAlignTwo extends CommandBase {
+    private final SwerveDrive swerve;
+    private final MuxedColorSensor colorSensor = MuxedColorSensor.getInstance();
+
+    public HangerAlignTwo(SwerveDrive swerveDrive) {
+        swerve = swerveDrive;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(swerveDrive);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return colorSensor.colorsMatch(colorSensor.getLeftAlignSensorColor(), TAPE_COLOR)&&
+                colorSensor.colorsMatch(colorSensor.getRightAlignSensorColor(), TAPE_COLOR);
+    }
+}
