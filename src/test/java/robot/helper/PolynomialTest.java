4dec9cec159dc3621844ba6bb1f01e2afc84ba2a
3LucasZ <lucas.zheng@warriorlife.net>
2022-02-24 16:06:33 -0800

    checkPolynomialIO unit test
    unit test success

--- src/test/java/robot/helper/PolynomialTest.java
+++ src/test/java/robot/helper/PolynomialTest.java
@@ -1,6 +1,7 @@
 package robot.helper;
 
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.helper.FileUtil;
 import frc.robot.helper.Polynomial;
 import org.junit.Before;
 import org.junit.Test;
@@ -30,5 +31,18 @@ public class PolynomialTest {
         Polynomial testPolynomial = new Polynomial(new double[]{3,2,5,6});
         //8*6 + 4*5 + 2*2 +3 = 75
         assertTrue(testPolynomial.getOutput(2)==75);
+    }
+
+    @Test
+    public void checkPolynomialIO(){
+        Polynomial testWritePolynomial = new Polynomial(new double[]{3,2,5,6});
+        FileUtil.writeObjectToFile("Polynomial.txt", testWritePolynomial);
+        Polynomial testReadPolynomial = (Polynomial) FileUtil.readObjectFromFile("Polynomial.txt");
+        assertTrue(testReadPolynomial.getOutput(2)==75);
+    }
+
+    @Test
+    public void checkPolynomialDefault(){
+        Polynomial testReadPolynomial;
     }
 }
