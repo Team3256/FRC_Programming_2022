93eb354452763e2783f477689a89e0c954800395
Rishi Wadhwa <rishi@wadhwas.us>
2022-02-26 14:25:44 -0800

    JavaDoc for ShooterPreset.java constructor

--- src/main/java/frc/robot/helper/shooter/ShooterPreset.java
+++ src/main/java/frc/robot/helper/shooter/ShooterPreset.java
@@ -4,6 +4,11 @@ public class ShooterPreset {
     public String presetName;
     public ShooterState currentState;
 
+    /**
+     * @param v velocity in rpm (velocity)
+     * @param t hoodAngle in radians (theta)
+     * @param name preset name to be given to the operator on selection
+     */
     public ShooterPreset(double v, double t, String name) {
         this.presetName = name;
         currentState = new ShooterState(v, t);
