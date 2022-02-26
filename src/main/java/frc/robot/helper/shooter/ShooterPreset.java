f15a9d26f48a5f164b9e0093361c7dc08d0acb08
Rishi Wadhwa <rishi@wadhwas.us>
2022-02-26 14:34:18 -0800

    ShooterPreset constructor parameter name updated

--- src/main/java/frc/robot/helper/shooter/ShooterPreset.java
+++ src/main/java/frc/robot/helper/shooter/ShooterPreset.java
@@ -5,12 +5,12 @@ public class ShooterPreset {
     public ShooterState currentState;
 
     /**
-     * @param v velocity in rpm (velocity)
-     * @param t hoodAngle in motorSensorUnits (theta)
+     * @param velocity velocity in rpm (velocity)
+     * @param hoodAngleTheta hoodAngle in motorSensorUnits (theta)
      * @param name preset name to be given to the operator on selection
      */
-    public ShooterPreset(double v, double t, String name) {
+    public ShooterPreset(double velocity, double hoodAngleTheta, String name) {
         this.presetName = name;
-        currentState = new ShooterState(v, t);
+        currentState = new ShooterState(velocity, hoodAngleTheta);
     }
 }
