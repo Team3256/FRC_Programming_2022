faaf8b978ce2f3cb4dd6f55fce3c3c2442fc2047
Rishi Wadhwa <rishi@wadhwas.us>
2022-02-26 13:12:12 -0800

    ShooterStat comments for clearification

--- src/main/java/frc/robot/helper/shooter/ShooterState.java
+++ src/main/java/frc/robot/helper/shooter/ShooterState.java
@@ -1,8 +1,8 @@
 package frc.robot.helper.shooter;
 
 public class ShooterState {
-    public double rpmVelocity;
-    public double hoodAngle;
+    public double rpmVelocity; // in rpm
+    public double hoodAngle; // in
 
     public ShooterState(double v, double t) {
         this.rpmVelocity = v;
