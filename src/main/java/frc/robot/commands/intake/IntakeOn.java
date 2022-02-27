b7266acbd9c160c117bf75c931a9d7123fa0f23e
Dylan Starink <dylan@starink.com>
2022-02-26 16:24:50 -0800

    Allow Intake to Reverse

--- src/main/java/frc/robot/commands/intake/IntakeOn.java
+++ src/main/java/frc/robot/commands/intake/IntakeOn.java
@@ -19,7 +19,7 @@ public class IntakeOn extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        intake.on();
+        intake.forwardOn();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
