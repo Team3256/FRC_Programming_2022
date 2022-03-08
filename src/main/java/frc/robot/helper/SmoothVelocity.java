2b79098dfb6a5d11a9726a06ef7610c4ce60715b
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2022-03-08 00:02:18 -0800

    fixes

--- src/main/java/frc/robot/helper/SmoothVelocity.java
+++ src/main/java/frc/robot/helper/SmoothVelocity.java
@@ -1,16 +1,8 @@
 package frc.robot.helper;
 
 public class SmoothVelocity {
-
-    public void SmoothVelocity() {
-    }
-
     public static double smoothVelocity(double currentVelocity, double targetVelocity, double acceleration, double time) {
-        double delta = targetVelocity - currentVelocity;
-        if (Math.abs(delta) < acceleration) {
-            return targetVelocity;
-        } else {
-            return currentVelocity + Math.signum(delta) * acceleration * time;
-        }
+        double currentAcceleration = (targetVelocity - currentVelocity)/time;
+        return currentVelocity + Math.signum(currentAcceleration) * Math.min(Math.abs(currentAcceleration), acceleration) * time;
     }
 }
