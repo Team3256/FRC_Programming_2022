e0a59d6ab0e0beef20d145e882945b0b4362cc55
3LucasZ <lucas.zheng@warriorlife.net>
2022-04-10 17:25:47 -0700

    Update GoToBallCommand.java

--- src/main/java/frc/robot/commands/GoToBallCommand.java
+++ src/main/java/frc/robot/commands/GoToBallCommand.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.hardware.BallTracker;
 import frc.robot.subsystems.SwerveDrive;
 
 public class GoToBallCommand extends CommandBase {
@@ -10,5 +12,17 @@ public class GoToBallCommand extends CommandBase {
         this.drivetrainSubsystem=drivetrainSubsystem;
     }
 
-    
+    @Override
+    public void execute(){
+        drivetrainSubsystem.drive(new ChassisSpeeds(BallTracker.getDx()*BallTracker.getDy(), BallTracker.getDy(), 0));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Math.abs(BallTracker.getDx())<dxThresh &&
+                Math.abs(BallTracker.getDy())<dyThresh;
+    }
+
+    double dxThresh = 0.000001;
+    double dyThresh = 0.000001;
 }
