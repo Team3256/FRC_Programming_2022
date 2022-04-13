dfb582f355cd89424032edcd2bb1d6aa3c9b5e9a
3LucasZ <lucas.j.zheng@gmail.com>
2022-04-12 19:21:58 -0700

    so much refactoring :)

---
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDSubsystem extends SubsystemBase {
+    private AddressableLED LED;
+    private AddressableLEDBuffer LEDBuffer;
+
+    public LEDSubsystem(){
+        LED = new AddressableLED(0);
+        LEDBuffer = new AddressableLEDBuffer(60);
+        LED.setLength(LEDBuffer.getLength());
+
+        LED.setData(LEDBuffer);
+        LED.start();
+    }
+
+    public void on(){
+        for (int i=0;i<LEDBuffer.getLength();i++){
+            LEDBuffer.setRGB(i,0,255,0);
+        }
+        LED.setData(LEDBuffer);
+    }
+    public void off(){
+        for (int i=0;i<LEDBuffer.getLength();i++){
+            LEDBuffer.setRGB(i,0,0,0);
+        }
+        LED.setData(LEDBuffer);
+    }
+}
